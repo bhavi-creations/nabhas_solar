c132e18848cde2321159d17326bbe4bedd0327f8
manimalladi <manimalladi05@gmail.com>
2026-03-24 18:51:04 -0800

    changes updated

--- get_in_touch.php
+++ get_in_touch.php
@@ -6,7 +6,7 @@
     <div class="container h-100 d-flex align-items-center justify-content-center">
       <div class="row w-100">
         <div class="col-12 text-center text-white">
-          <h1 class="about-new-section-title fw-bold">Our Projects</h1>
+          <h1 class="about-new-section-title fw-bold"> Get a Free Quote</h1>
           <div class="about-new-section-divider mx-auto"></div>
           <!-- <p class="about-new-section-description fs-5 mt-3">
                         Crafting Excellence Through Unique IT Services — <br>
